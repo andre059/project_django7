e7f73d7904647e0fb84e9f1eaab48a5f05721df4
andre059 <andreyperm159@mail.ru>
2023-11-18 13:26:49 +0500

    2 (переделал через ViewSets)

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
+router = DefaultRouter()
+router.register(r'lesson', UserViewSet, basename='lesson')
 
 urlpatterns = [
-    path('users/create/', UserCreateAPIView.as_view(), name='users-create'),
-    path('users/', UserListAPIView.as_view(), name='users-list'),
-    path('users/<int:pk>/', UserRetrieveAPIView.as_view(), name='users-get'),
-    path('users/update/<int:pk>/', UserUpdateAPIView.as_view(), name='users-update'),
-    path('users/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='users-delete'),
+    path('', include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,27 +1,8 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from users.serliazers import UserSerializer
-from  users.models import User
+from users.models import User
 
 
-class UserCreateAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
-
-
-class UserListAPIView(generics.ListAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    
-
-class UserRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-
-class UserUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-
-class UserDestroyAPIView(generics.DestroyAPIView):
-    queryset = User.objects.all()
-    
