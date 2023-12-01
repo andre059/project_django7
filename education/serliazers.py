e2c8d257014c9d535ef432cbaee512177420238f
andre059 <andreyperm159@mail.ru>
2023-12-01 23:55:14 +0500

    8 (stripe)

--- education/serliazers.py
+++ education/serliazers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from rest_framework.permissions import IsAuthenticated
 
 from education.models import Lesson, Course
+from education.services import convert_currencies
 from education.validators import LinkVideoValidator
 
 
@@ -10,18 +10,20 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
-        # permission_classes = [IsAuthenticated]  # требует аутентификации пользователя для доступа к объектам модели
         validators = [LinkVideoValidator('video_link')]  # проверка этого поля на регулярное выражение
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)  # Подгрузка данных из связанных моделей
-
-    def get_lesson_count(self, obj):
-        return Lesson.objects.filter(course=obj.id).count()
+    usd_prise = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
-        # permission_classes = [IsAuthenticated]  # требует аутентификации пользователя для доступа к объектам модели
+
+    def get_lesson_count(self, obj):
+        return Lesson.objects.filter(course=obj.id).count()
+
+    def get_usd_prise(self, instance):
+        return convert_currencies(instance.price)
